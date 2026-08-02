f7fa5fb4bb38f7e1c582c0dbdef5912f5220b28a
Taner Öksüz <taner.oeksuez@outlook.de>
2026-08-02 09:53:37 +0200

    state machine in fetch unit optimized.

--- verilog/rtl/fetch_stage.sv
+++ verilog/rtl/fetch_stage.sv
@@ -64,15 +64,36 @@ module fetch_stage (
 
     case (v.state)
       IDLE: begin
+        v.state = BUSY;
+        v.valid = 1;
         v.stall = 1;
       end
       BUSY: begin
-        if (v.ready == 0) begin
+        if (v.ready == 1) begin
+          v.state = BUSY;
+          v.valid = 1;
+        end else if (v.spec == 1) begin
+          v.state = INVALID;
+          v.valid = 0;
+          v.stall = 1;
+        end else begin
+          v.state = BUSY;
+          v.valid = 0;
           v.stall = 1;
         end
       end
       INVALID: begin
-        v.stall = 1;
+        if (v.ready == 1) begin
+          v.state = BUSY;
+          v.ready = 0;
+          v.valid = 1;
+          v.stall = 1;
+        end else begin
+          v.state = INVALID;
+          v.ready = 0;
+          v.valid = 0;
+          v.stall = 1;
+        end
       end
       default: begin
       end
@@ -99,39 +120,6 @@ module fetch_stage (
     end
 
     v.ipc1 = v.ipc0 + 4;
-
-    case (v.state)
-      IDLE: begin
-        if (clear == 0) begin
-          v.state = BUSY;
-          v.valid = 1;
-        end
-      end
-      BUSY: begin
-        if (v.ready == 1) begin
-          v.state = BUSY;
-          v.valid = 1;
-        end else if (v.spec == 1) begin
-          v.state = INVALID;
-          v.valid = 0;
-        end else begin
-          v.state = BUSY;
-          v.valid = 0;
-        end
-      end
-      INVALID: begin
-        if (v.ready == 1) begin
-          v.state = BUSY;
-          v.valid = 1;
-        end else begin
-          v.state = INVALID;
-          v.valid = 0;
-        end
-        v.ready = 0;
-      end
-      default: begin
-      end
-    endcase
 
     buffer_in.pc0   = r.ipc0;
     buffer_in.pc1   = r.ipc1;
